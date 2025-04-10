01515e766a6bd3834a1ff98d706733633bcc4a48
Dmitrii Gutman <dgtmn@protonmail.com>
2025-04-10 13:25:58 +0900

    fix api key provider

--- flake.nix
+++ flake.nix
@@ -4,29 +4,39 @@
   };
 
   outputs = { nixpkgs, self }:
-    let forEachSystem = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system: f nixpkgs.legacyPackages.${system});
-    in {
+    let
+      forEachSystem = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-darwin" ] (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
       packages = forEachSystem
-        (pkgs: {
-          default = pkgs.stdenv.mkDerivation
-            {
+        (pkgs:
+          let
+            default = pkgs.stdenv.mkDerivation {
               pname = "llmpeg";
               version = "0.0.0";
               src = ./llmpeg;
               dontUnpack = true;
+              nativeBuildInputs = [ ];
               buildInputs = with pkgs; [
                 curl
                 ffmpeg-full
               ];
-              nativeBuildInputs = with pkgs; [ makeWrapper ];
               installPhase = ''
                 mkdir -p $out/bin
                 install -D $src $out/bin/llmpeg
-                wrapProgram $out/bin/llmpeg --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.curl pkgs.ffmpeg-full]} --run 'export NEBIUS_API_KEY="$(cat $NEBIUS_API_KEY_PATH)"'
               '';
-              NEBIUS_API_KEY_PATH = "";
+              passthru = { inherit withApiKey; };
             };
-        });
+
+            withApiKey = keyPath: default.overrideAttrs (prev: {
+              nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.makeWrapper ];
+              postFixup = ''
+                wrapProgram $out/bin/llmpeg \
+                  --run 'export NEBIUS_API_KEY="$(cat ${keyPath})"'
+              '';
+            });
+          in
+          { inherit default; });
 
       overlays.default = final: prev: {
         llmpeg-nebius = self.packages.${prev.system}.default;
